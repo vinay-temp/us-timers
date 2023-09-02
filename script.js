2f8bc6b08a70822a21b610b697ad5f57eecf8fb4
vinay-temp <117427777+vinay-temp@users.noreply.github.com>
2023-09-02 18:48:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // (Year, Month (0-11), Day, Hour, Minute, Second)
 const data = [
   ["Vinay", new Date(2023, 7, 24, 13, 35, 0)],
-  ["Ajay", new Date()],
-  ["Priyam", new Date()],
+  ["Ajay", new Date(2023, 7, 30, 13, 35, 0)],
+  ["Priyam", new Date(2023, 7, 13, 13, 35, 0)],
 ].sort((a, b) => {
   return a[1] - b[1];
 });
@@ -29,14 +29,12 @@ for (let i = 0; i < data.length; i++) {
 function updateTimer(startTime) {
   const timeDiff = new Date() - startTime;
 
-  const seconds = Math.floor(timeDiff / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
+  const secs = Math.floor(timeDiff / 1000);
+  const mins = Math.floor(secs / 60);
+  const hours = Math.floor(mins / 60);
   const days = Math.floor(hours / 24);
 
-  return `${days} days ${hours % 24} hours ${minutes % 60} minutes ${
-    seconds % 60
-  } seconds`;
+  return `${days} days ${hours % 24} hours ${mins % 60} mins ${secs % 60} secs`;
 }
 
 function updateAll() {
