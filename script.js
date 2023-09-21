013201d0530a910aced2db2711aab572c90094b0
vinay-temp <117427777+vinay-temp@users.noreply.github.com>
2023-09-21 20:46:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ for (let i = 0; i < data.length; i++) {
   th.textContent = (i + 1).toString();
   tdName.textContent = data[i][0];
   if (i == 0) {
-    tdName.textContent += " 👑";
+    tdName.textContent += " 👑 KING 👑";
   }
   if (i == data.length - 1) {
     tdName.textContent += " Mutthal";
