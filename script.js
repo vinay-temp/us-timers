2e2c607de03c7b5697697b81587b486f3121d704
vinay-temp <117427777+vinay-temp@users.noreply.github.com>
2023-09-27 18:36:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const data = [
   ["Priyam", new Date(2023, 8, 9, 3, 30, 0)],
   ["Shivanshu", new Date(2023, 8, 19, 15, 0, 0)],
   ["Harshit", new Date(2023, 8, 19, 18, 0, 0)],
-  ["Shukla", new Date(2023, 8, 13, 21, 0, 0)],
-  ["Anubhav", new Date(2023, 7, 13, 0, 0, 0)],
 ].sort((a, b) => {
   return a[1] - b[1];
 });
